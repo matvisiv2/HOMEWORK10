cbe13d4c3734ec296feb7bf4e80769a55b0d8689
matvisiv2 <matvisiv2@gmail.com>
2025-09-11 14:08:40 +0300

    task02

--- js/HOMEWORK10.js
+++ js/HOMEWORK10.js
@@ -15,6 +15,7 @@ function task01() {
     console.log(z); // [Bob]
 }
 
+
 // 2.
 // Вихідний код:
 // let data = {
@@ -27,6 +28,22 @@ function task01() {
 // console.log(name4); // "Bob"
 // console.log(age4); // 26
 // 	Допишіть код використовуючи деструктурування, щоб в консолі браузера з'явилися рядки, які написані в коментарях.
+
+function task02() {
+    let data = {
+        names: ["Sam", "Tom", "Ray", "Bob"],
+        ages: [20, 24, 22, 26],
+    };
+    let {
+        names: [, name2, , name4],
+        ages: [, age2, , age4]
+    } = data;
+
+    console.log(name2); // "Tom"
+    console.log(age2); // 24
+    console.log(name4); // "Bob"
+    console.log(age4); // 26
+}
 
 
 // 3. Напишіть функцію mul(), яка приймає будь-яку кількість параметрів різного типу і повертає добуток параметрів типу Number.
